24436e5f8b93f80b7621ef7cbc927da17da33ad5
PaulLuv <bao.lu@mobvista.com>
2019-09-09 18:05:37 +0800

    fix AddTwoNumbers

---
+++ app/src/main/java/com/paul/leetcode/AddTwoNumbers.java
@@ -0,0 +1,69 @@
+package com.paul.leetcode;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class AddTwoNumbers {
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    /**
+     * 执行结果：通过
+     * 显示详情
+     *      执行用时 : 4 ms, 在所有 Java 提交中击败了 97.19% 的用户
+     *      内存消耗 : 48.3 MB, 在所有 Java 提交中击败了 65.04% 的用户
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        int add = 0;
+        ListNode result = null;
+        ListNode currentNode = null;
+        while (l1 != null && l2 != null) {
+            if (currentNode == null) {
+                currentNode = new ListNode((l1.val + l2.val + add) % 10);
+                result = currentNode;
+            }else {
+                currentNode.next = new ListNode((l1.val + l2.val + add) % 10);
+                currentNode = currentNode.next;
+            }
+            add = (l1.val + l2.val + add) / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l1 != null){
+            currentNode.next = new ListNode((l1.val + add) % 10);
+            add = (l1.val + add) / 10;
+            l1 = l1.next;
+            currentNode = currentNode.next;
+        }
+        while (l2 != null){
+            currentNode.next = new ListNode((l2.val + add) % 10);
+            add = (l2.val + add) / 10;
+            l2 = l2.next;
+            currentNode = currentNode.next;
+        }
+        if (add != 0){
+            currentNode.next = new ListNode(add);
+        }
+        return result;
+    }
+}
--- app/src/test/java/com/paul/jcenter/ExampleUnitTest.java
+++ app/src/test/java/com/paul/jcenter/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.paul.jcenter;
 
+import com.paul.leetcode.AddTwoNumbers;
 import com.paul.leetcode.TwoSum;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -23,5 +27,37 @@ public class ExampleUnitTest {
         int[] nums1 = {-2, -1, 0, 1, 4, 5, 8};
         assertArrayEquals( new int[]{1,7},TwoSum.solution1(nums,7));
         assertArrayEquals( new int[]{0,6},TwoSum.solution1(nums1,6));
+    }
+
+    @Test
+    public void addTwoNumbers(){
+        AddTwoNumbers.ListNode l1 = new AddTwoNumbers.ListNode(2);
+        AddTwoNumbers.ListNode l1next = new AddTwoNumbers.ListNode(4);
+        AddTwoNumbers.ListNode l1nextnext = new AddTwoNumbers.ListNode(3);
+        l1.next = l1next;
+        l1next.next = l1nextnext;
+
+        AddTwoNumbers.ListNode l2 = new AddTwoNumbers.ListNode(5);
+        AddTwoNumbers.ListNode l2next = new AddTwoNumbers.ListNode(6);
+        AddTwoNumbers.ListNode l2nextnext = new AddTwoNumbers.ListNode(4);
+        l2.next = l2next;
+        l2next.next = l2nextnext;
+
+        AddTwoNumbers.ListNode l3 = new AddTwoNumbers.ListNode(7);
+        AddTwoNumbers.ListNode l3next = new AddTwoNumbers.ListNode(0);
+        AddTwoNumbers.ListNode l3nextnext = new AddTwoNumbers.ListNode(8);
+        l3.next = l3next;
+        l3next.next = l3nextnext;
+
+        assertArrayEquals(listNodeToList(l3).toArray(),listNodeToList(AddTwoNumbers.addTwoNumbers1(l1,l2)).toArray());
+    }
+
+    public List listNodeToList(AddTwoNumbers.ListNode listNode){
+        List<Integer> list = new ArrayList<>();
+        while (listNode != null){
+            list.add(listNode.val);
+            listNode = listNode.next;
+        }
+        return list;
     }
 }
